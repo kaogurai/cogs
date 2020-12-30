aeffff7f39e4028c7c6d4a84cc681029829b01f4
alec <{ID}+{username}@users.noreply.github.com>
2020-12-29 20:28:48 -0500

    first part

--- brainshop/brainshop.py
+++ brainshop/brainshop.py
@@ -2,18 +2,16 @@ from redbot.core import commands
 import aiohttp
 
 class Brainshop(commands.Cog):
-    """brainshop.ai cog - for this to work, you will need to get a brain from https://brainshop.ai and [p]set api brainshop.ai brain_id <id> brain_key <key>"""
+    """brainshop.ai cog - for this to work, you will need to get a brain from https://brainshop.ai and [p]set api brainshopai brain_id <id> brain_key <key>"""
 
     @commands.command()
     async def talk(self, ctx):
         """Talk to a robot!"""
         # add code here
-        brain_info = await self.bot.get_shared_api_tokens(brainshop.ai)
-        if brainshop.ai.get("brain_id") is None:
+        brain_info = await self.bot.get_shared_api_tokens(brainshopai)
+        if brainshopai.get("brain_id") is None:
             return await ctx.send("The brain id has not been set.")
-        if brainshop.ai.get("brain_key") is None:
+        if brainshopai.get("brain_key") is None:
             return await ctx.send("The brain key has not been set.")
-
-
 
         await ctx.send("no")
