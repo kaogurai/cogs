f955cf8e1687a0e8e5f86ff4e7522c8fe2b4e223
alec <{ID}+{username}@users.noreply.github.com>
2020-12-29 20:41:00 -0500

    forgot quotes

--- brainshop/brainshop.py
+++ brainshop/brainshop.py
@@ -8,7 +8,7 @@ class Brainshop(commands.Cog):
     async def talk(self, ctx, message: str):
         """Talk to a robot!"""
         # add code here
-        brain_info = await ctx.bot.get_shared_api_tokens(brainshopai)
+        brain_info = await ctx.bot.get_shared_api_tokens("brainshopai")
         if brain_info.get("brain_id") is None:
             return await ctx.send("The brain id has not been set.")
         if brain_info.get("brain_key") is None:
