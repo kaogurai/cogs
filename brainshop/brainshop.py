8a944ebba1e2773664b09b63fbcb2caf0bb67083
alec <{ID}+{username}@users.noreply.github.com>
2020-12-29 20:54:56 -0500

    test

--- brainshop/brainshop.py
+++ brainshop/brainshop.py
@@ -8,10 +8,14 @@ class Brainshop(commands.Cog):
     async def talk(self, ctx, message: str):
         """Talk to a robot!"""
         # add code here
+
         brain_info = await ctx.bot.get_shared_api_tokens("brainshopai")
         if brain_info.get("brain_id") is None:
             return await ctx.send("The brain id has not been set.")
         if brain_info.get("brain_key") is None:
             return await ctx.send("The brain key has not been set.")
 
-        await ctx.send("no")
+        url = "http://api.brainshop.ai/get?bid={brain_id}&key={brain_key}&uid=[uid]&msg=[msg]"
+
+
+        await ctx.send(url)
