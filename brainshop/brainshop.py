a1407cae865200b749254d45284b14132df84dcc
alec <{ID}+{username}@users.noreply.github.com>
2020-12-29 20:15:22 -0500

    .

--- brainshop/brainshop.py
+++ brainshop/brainshop.py
@@ -1,7 +1,7 @@
 from redbot.core import commands
 
 class Brainshop(commands.Cog):
-    """brainshop.ai cog"""
+    """brainshop.ai cog - for this to work, you will need to get a brain from https://brainshop.ai and [p]set api brainshop.ai brain_id <id> brain_key <key>"""
 
     @commands.command()
     async def mycom(self, ctx):
