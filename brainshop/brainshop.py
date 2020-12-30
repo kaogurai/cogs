7d99dbb9dc2f596e638dc42e96c9ee4d9406a3c3
alec <{ID}+{username}@users.noreply.github.com>
2020-12-29 20:36:59 -0500

    first part

--- brainshop/brainshop.py
+++ brainshop/brainshop.py
@@ -5,10 +5,10 @@ class Brainshop(commands.Cog):
     """brainshop.ai cog - for this to work, you will need to get a brain from https://brainshop.ai and [p]set api brainshopai brain_id <id> brain_key <key>"""
 
     @commands.command()
-    async def talk(self, ctx, user: str):
+    async def talk(self, ctx, message: str):
         """Talk to a robot!"""
         # add code here
-        brain_info = await self.bot.get_shared_api_tokens(brainshopai)
+        brain_info = await ctx.bot.get_shared_api_tokens(brainshopai)
         if brainshopai.get("brain_id") is None:
             return await ctx.send("The brain id has not been set.")
         if brainshopai.get("brain_key") is None:
