4516b6de429ab540e930127ff05c49f1a7011268
alec <{ID}+{username}@users.noreply.github.com>
2021-01-01 20:21:06 -0500

    ???

--- brainshop/brainshop.py
+++ brainshop/brainshop.py
@@ -1,4 +1,4 @@
-from redbot.core import commands
+from redbot.core import commands, config, checks
 import aiohttp 
 import urllib.parse
 
@@ -24,3 +24,41 @@ class Brainshop(commands.Cog):
             async with session.get(url) as request:
                 response = await request.json()
                 await ctx.send(response['cnt'])
+        
+    @commands.group()
+    @commands.mod()
+    async def brainset(self, ctx):
+        """Configure the AI! This does not currently work."""
+         
+    
+    @brainset.group()
+    @commands.admin()
+    async def channel(self, ctx):
+        """Manage the channels the AI talks in."""
+
+    @channel.command()
+    async def add(self, ctx):
+        """Add a channel for the AI to talk in."""
+    @channel.command()
+    async def remove(self, ctx):
+        """Remove a channel for the AI to talk in."""
+    @channel.command()
+    async def list(self, ctx):
+        """View all the channels that the AI will talk in."""
+         
+    
+    @brainset.group()
+    @commands.mod()
+    async def blacklist(self, ctx):
+        """Blacklist people from using the AI."""
+
+    @blacklist.command()
+    async def add(self, ctx):
+        """Add people to the blacklist."""
+    @blacklist.command()
+    async def remove(self, ctx):
+        """Remove people from the blacklist."""
+    @blacklist.command()
+    async def list(self, ctx):
+        """View the blacklist."""
+         
