ec8a2593794ee98dd7f37bb26022644c885916a8
alec <{ID}+{username}@users.noreply.github.com>
2021-01-05 19:54:50 -0500

    ??

--- randomcmds/randomcmds.py
+++ randomcmds/randomcmds.py
@@ -7,13 +7,18 @@ class Randomcmds(commands.Cog):
 
     @commands.command()
     async def asia(self, ctx):
-       await ctx.send("asia is the best person on this earth and loves videos of emo kids dancing")
-       await ctx.send("https://cdn.discordapp.com/attachments/768663090337677315/795133511673053225/emokidsyummy.mp4")
+        """emo kids lover"""
+        await ctx.send("asia is the best person on this earth and loves videos of emo kids dancing")
+        await ctx.send("https://cdn.discordapp.com/attachments/768663090337677315/795133511673053225/emokidsyummy.mp4")
 
     @commands.command()
     async def maddie(self, ctx):
-        embed=discord.Embed(description="maddie is a cool cat + is emotionally attached to this cat’s birthday party :revolving_hearts::revolving_hearts::revolving_hearts::revolving_hearts:", color=11985904, image = "https://cdn.discordapp.com/attachments/768663090337677315/796118254128332820/image0.jpg")
+        """cool cat"""
+        embed=discord.Embed(description="maddie is a cool cat + is emotionally attached to this cat’s birthday party :revolving_hearts::revolving_hearts::revolving_hearts::revolving_hearts:", color=11985904)
+        embed.set_image(url = "https://cdn.discordapp.com/attachments/768663090337677315/796118254128332820/image0.jpg")
         await ctx.send(embed=embed)
+
     @commands.command()
     async def oofchair(self, ctx):
+       """cool bot dev"""
        await ctx.send("oof is p cool :) he's also a bot developer! check out his bot here: http://pwnbot.xyz/")
