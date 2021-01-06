74b104a0270043c9dfae52e4df2a884324a75d6d
alec <{ID}+{username}@users.noreply.github.com>
2021-01-05 19:49:37 -0500

    ??

--- randomcmds/randomcmds.py
+++ randomcmds/randomcmds.py
@@ -7,16 +7,13 @@ class Randomcmds(commands.Cog):
 
     @commands.command()
     async def asia(self, ctx):
-       ctx.send("asia is the best person on this earth and loves videos of emo kids dancing")
-       ctx.send("https://cdn.discordapp.com/attachments/768663090337677315/795133511673053225/emokidsyummy.mp4")
+       await ctx.send("asia is the best person on this earth and loves videos of emo kids dancing")
+       await ctx.send("https://cdn.discordapp.com/attachments/768663090337677315/795133511673053225/emokidsyummy.mp4")
 
     @commands.command()
     async def maddie(self, ctx):
         embed=discord.Embed(description="maddie is a cool cat + is emotionally attached to this cat’s birthday party :revolving_hearts::revolving_hearts::revolving_hearts::revolving_hearts:", color=11985904, image = "https://cdn.discordapp.com/attachments/768663090337677315/796118254128332820/image0.jpg")
-        ctx.send(embed=embed)
+        await ctx.send(embed=embed)
     @commands.command()
     async def oofchair(self, ctx):
-       ctx.send("oof is p cool :) he's also a bot developer! check out his bot here: http://pwnbot.xyz/")
-    @commands.command()
-    async def nicki(self, ctx):
-       ctx.send("ok")
+       await ctx.send("oof is p cool :) he's also a bot developer! check out his bot here: http://pwnbot.xyz/")
