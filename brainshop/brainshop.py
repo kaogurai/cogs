e8872bde9135b741cc046989503295d6372fa6e3
alec <{ID}+{username}@users.noreply.github.com>
2021-01-25 22:18:16 -0500

    start to config

--- brainshop/brainshop.py
+++ brainshop/brainshop.py
@@ -38,26 +38,35 @@ class Brainshop(commands.Cog):
     @channel.command()
     async def add(self, ctx):
         """Add a channel for the AI to talk in."""
+        if not channel:
+            channel = ctx.channel
+        perms = await self._channel_perm_checker(channel)
+        if (not perms):
+        msg = f"Sorry, I don't have permission to send messages in that channel. "
+        return await ctx.send(msg)
+
     @channel.command()
     async def remove(self, ctx):
         """Remove a channel for the AI to talk in."""
+
     @channel.command()
     async def list(self, ctx):
         """View all the channels that the AI will talk in."""
-         
-    
-    @brainset.group()
-    @commands.mod()
-    async def blacklist(self, ctx):
-        """Blacklist people from using the AI."""
 
-    @blacklist.command()
-    async def add(self, ctx):
-        """Add people to the blacklist."""
-    @blacklist.command()
-    async def remove(self, ctx):
-        """Remove people from the blacklist."""
-    @blacklist.command()
-    async def list(self, ctx):
-        """View the blacklist."""
+# This is just the plan for these commands - they aren't implemented yet 
+    
+ #   @brainset.group()
+ #   @commands.mod()
+ #   async def blacklist(self, ctx):
+ #       """Blacklist people from using the AI."""
+
+ #  @blacklist.command()
+ #   async def add(self, ctx):
+ #       """Add people to the blacklist."""
+ #   @blacklist.command()
+ #   async def remove(self, ctx):
+ #       """Remove people from the blacklist."""
+ #   @blacklist.command()
+ #   async def list(self, ctx):
+ #       """View the blacklist."""
          
