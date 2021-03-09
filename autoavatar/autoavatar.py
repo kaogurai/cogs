b6b03e5ce9a21eb3bf0a43ec69e77202330e5a05
alec <50505980+aleclol@users.noreply.github.com>
2021-03-08 19:26:49 -0500

    New Avatar Every Hour

--- autoavatar/autoavatar.py
+++ autoavatar/autoavatar.py
@@ -13,17 +13,13 @@ class AutoAvatar(commands.Cog):
         self.config = Config.get_conf(self, identifier=696969696969494)
         default_global = { "avatars": [ 'https://avatars.githubusercontent.com/u/23690422?s=400&v=4'] }
         self.config.register_global(**default_global)
-        self.avatar_task = asyncio.create_task(self.wait_for_avatar())
-
-    def cog_unload(self):
-        self.avatar_task.cancel()
 
     async def wait_for_avatar(self):
         await self.bot.wait_until_red_ready()
         while True:
             try:
                 await self.change_avatar()
-                await asyncio.sleep(10800)
+                await asyncio.sleep(3600)
             except asyncio.CancelledError:
                 break
     
