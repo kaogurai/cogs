c59b02ebf2e035feb27c15613eac95b2edb75f30
alec <50505980+aleclol@users.noreply.github.com>
2021-04-16 13:32:47 -0400

    ugh

--- kaotools/kaotools.py
+++ kaotools/kaotools.py
@@ -41,7 +41,7 @@ class KaoTools(commands.Cog):
         **Hey there!** <a:bounce:778449468717531166>
         My prefixes in this server are {humanize_list(prefixes)}
         You can also mention me as a prefix!
-        You may type `{(await self.bot.get_prefix(message.channel))[2]}help` to view all commands!
+        Typing`{(await self.bot.get_prefix(message.channel))[2]}help` will show all commands!
         Need some help? Join my [support server!](https://discord.gg/p6ehU9qhg8)
         Looking to invite me? [Click here!](https://discord.com/oauth2/authorize?client_id={message.guild.me.id}&permissions=6441922047&scope=bot+applications.commands)
         """)
