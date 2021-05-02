ec6b520269e1166738deb075071e1b8f9d87b051
alec <50505980+aleclol@users.noreply.github.com>
2021-05-02 19:47:27 -0400

    lol

--- getemoji/getemoji.py
+++ getemoji/getemoji.py
@@ -5,13 +5,15 @@ import discord
 
 class GetEmoji(commands.Cog):
     def __init__(self):
+        self.session = aiohttp.ClientSession()
         self.config = Config.get_conf(
             self, identifier=6574839238457654839284756548392384756
         )
         default_global = {"url": "http://localhost:6969/"}
         self.config.register_global(**default_global)
 
-    # im cringing looking at this rn, i'll eventually get to rewriting it <3
+    def cog_unload(self):
+        self.bot.loop.create_task(self.session.close())
 
     @commands.command()
     @commands.is_owner()
@@ -27,336 +29,115 @@ class GetEmoji(commands.Cog):
 
     @getemoji.command()
     @commands.bot_has_permissions(embed_links=True)
-    async def apple(self, ctx, emoji: str):
+    async def apple(self, ctx, *, emoji: str):
         """get an image of a apple emoji"""
-        await ctx.trigger_typing()
-        url = await self.config.url()
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url + "emoji/image/apple/" + emoji) as request:
-                    if request.status == 404:
-                        return await ctx.send(
-                            "Sorry, I couldn't find that emoji from my API."
-                        )
-                    response = await request.json()
-                    embed = discord.Embed(color=await ctx.embed_colour())
-                    embed.set_image(url=response["url"])
-                    await ctx.send(embed=embed)
-        except aiohttp.ClientConnectionError:
-            await ctx.send("Sorry, the API isn't set up correctly.")
+        await self.get_emoji(ctx, "apple", emoji)
 
     @getemoji.command()
     @commands.bot_has_permissions(embed_links=True)
-    async def google(self, ctx, emoji: str):
+    async def google(self, ctx, *, emoji: str):
         """get an image of a google emoji"""
-        await ctx.trigger_typing()
-        url = await self.config.url()
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url + "emoji/image/google/" + emoji) as request:
-                    if request.status == 404:
-                        return await ctx.send(
-                            "Sorry, I couldn't find that emoji from my API."
-                        )
-                    response = await request.json()
-                    embed = discord.Embed(color=await ctx.embed_colour())
-                    embed.set_image(url=response["url"])
-                    await ctx.send(embed=embed)
-        except aiohttp.ClientConnectionError:
-            await ctx.send("Sorry, the API isn't set up correctly.")
+        await self.get_emoji(ctx, "google", emoji)
 
     @getemoji.command()
     @commands.bot_has_permissions(embed_links=True)
-    async def samsung(self, ctx, emoji: str):
+    async def samsung(self, ctx, *, emoji: str):
         """get an image of a samsung emoji"""
-        await ctx.trigger_typing()
-        url = await self.config.url()
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url + "emoji/image/samsung/" + emoji) as request:
-                    if request.status == 404:
-                        return await ctx.send(
-                            "Sorry, I couldn't find that emoji from my API."
-                        )
-                    response = await request.json()
-                    embed = discord.Embed(color=await ctx.embed_colour())
-                    embed.set_image(url=response["url"])
-                    await ctx.send(embed=embed)
-        except aiohttp.ClientConnectionError:
-            await ctx.send("Sorry, the API isn't set up correctly.")
+        await self.get_emoji(ctx, "samsung", emoji)
 
     @getemoji.command()
     @commands.bot_has_permissions(embed_links=True)
-    async def microsoft(self, ctx, emoji: str):
+    async def microsoft(self, ctx, *, emoji: str):
         """get an image of a microsoft emoji"""
-        await ctx.trigger_typing()
-        url = await self.config.url()
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(
-                    url + "emoji/image/microsoft/" + emoji
-                ) as request:
-                    if request.status == 404:
-                        return await ctx.send(
-                            "Sorry, I couldn't find that emoji from my API."
-                        )
-                    response = await request.json()
-                    embed = discord.Embed(color=await ctx.embed_colour())
-                    embed.set_image(url=response["url"])
-                    await ctx.send(embed=embed)
-        except aiohttp.ClientConnectionError:
-            await ctx.send("Sorry, the API isn't set up correctly.")
+        await self.get_emoji(ctx, "microsoft", emoji)
 
     @getemoji.command()
     @commands.bot_has_permissions(embed_links=True)
-    async def whatsapp(self, ctx, emoji: str):
+    async def whatsapp(self, ctx, *, emoji: str):
         """get an image of a whatsapp emoji"""
-        await ctx.trigger_typing()
-        url = await self.config.url()
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(
-                    url + "emoji/image/whatsapp/" + emoji
-                ) as request:
-                    if request.status == 404:
-                        return await ctx.send(
-                            "Sorry, I couldn't find that emoji from my API."
-                        )
-                    response = await request.json()
-                    embed = discord.Embed(color=await ctx.embed_colour())
-                    embed.set_image(url=response["url"])
-                    await ctx.send(embed=embed)
-        except aiohttp.ClientConnectionError:
-            await ctx.send("Sorry, the API isn't set up correctly.")
+        await self.get_emoji(ctx, "whatsapp", emoji)
 
     @getemoji.command(aliases=["discord"])
     @commands.bot_has_permissions(embed_links=True)
-    async def twitter(self, ctx, emoji: str):
+    async def twitter(self, ctx, *, emoji: str):
         """get an image of a twitter/discord emoji"""
-        await ctx.trigger_typing()
-        url = await self.config.url()
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url + "emoji/image/twitter/" + emoji) as request:
-                    if request.status == 404:
-                        return await ctx.send(
-                            "Sorry, I couldn't find that emoji from my API."
-                        )
-                    response = await request.json()
-                    embed = discord.Embed(color=await ctx.embed_colour())
-                    embed.set_image(url=response["url"])
-                    await ctx.send(embed=embed)
-        except aiohttp.ClientConnectionError:
-            await ctx.send("Sorry, the API isn't set up correctly.")
+        await self.get_emoji(ctx, "twitter", emoji)
 
     @getemoji.command()
     @commands.bot_has_permissions(embed_links=True)
-    async def facebook(self, ctx, emoji: str):
+    async def facebook(self, ctx, *, emoji: str):
         """get an image of a facebook emoji"""
-        await ctx.trigger_typing()
-        url = await self.config.url()
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(
-                    url + "emoji/image/facebook/" + emoji
-                ) as request:
-                    if request.status == 404:
-                        return await ctx.send(
-                            "Sorry, I couldn't find that emoji from my API."
-                        )
-                    response = await request.json()
-                    embed = discord.Embed(color=await ctx.embed_colour())
-                    embed.set_image(url=response["url"])
-                    await ctx.send(embed=embed)
-        except aiohttp.ClientConnectionError:
-            await ctx.send("Sorry, the API isn't set up correctly.")
+        await self.get_emoji(ctx, "facebook", emoji)
 
     @getemoji.command()
     @commands.bot_has_permissions(embed_links=True)
-    async def joypixels(self, ctx, emoji: str):
+    async def joypixels(self, ctx, *, emoji: str):
         """get an image of a joypixels emoji"""
-        await ctx.trigger_typing()
-        url = await self.config.url()
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(
-                    url + "emoji/image/joypixels/" + emoji
-                ) as request:
-                    if request.status == 404:
-                        return await ctx.send(
-                            "Sorry, I couldn't find that emoji from my API."
-                        )
-                    response = await request.json()
-                    embed = discord.Embed(color=await ctx.embed_colour())
-                    embed.set_image(url=response["url"])
-                    await ctx.send(embed=embed)
-        except aiohttp.ClientConnectionError:
-            await ctx.send("Sorry, the API isn't set up correctly.")
+        await self.get_emoji(ctx, "joypixels", emoji)
 
     @getemoji.command()
     @commands.bot_has_permissions(embed_links=True)
-    async def openmoji(self, ctx, emoji: str):
+    async def openmoji(self, ctx, *, emoji: str):
         """get an image of a openmoji emoji"""
-        await ctx.trigger_typing()
-        url = await self.config.url()
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(
-                    url + "emoji/image/openmoji/" + emoji
-                ) as request:
-                    if request.status == 404:
-                        return await ctx.send(
-                            "Sorry, I couldn't find that emoji from my API."
-                        )
-                    response = await request.json()
-                    embed = discord.Embed(color=await ctx.embed_colour())
-                    embed.set_image(url=response["url"])
-                    await ctx.send(embed=embed)
-        except aiohttp.ClientConnectionError:
-            await ctx.send("Sorry, the API isn't set up correctly.")
+        await self.get_emoji(ctx, "openmoji", emoji)
 
     @getemoji.command()
     @commands.bot_has_permissions(embed_links=True)
-    async def emojidex(self, ctx, emoji: str):
+    async def emojidex(self, ctx, *, emoji: str):
         """get an image of a emojidex emoji"""
-        await ctx.trigger_typing()
-        url = await self.config.url()
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(
-                    url + "emoji/image/emojidex/" + emoji
-                ) as request:
-                    if request.status == 404:
-                        return await ctx.send(
-                            "Sorry, I couldn't find that emoji from my API."
-                        )
-                    response = await request.json()
-                    embed = discord.Embed(color=await ctx.embed_colour())
-                    embed.set_image(url=response["url"])
-                    await ctx.send(embed=embed)
-        except aiohttp.ClientConnectionError:
-            await ctx.send("Sorry, the API isn't set up correctly.")
+        await self.get_emoji(ctx, "emojidex", emoji)
 
     @getemoji.command()
     @commands.bot_has_permissions(embed_links=True)
-    async def lg(self, ctx, emoji: str):
+    async def lg(self, ctx, *, emoji: str):
         """get an image of a lg emoji"""
-        await ctx.trigger_typing()
-        url = await self.config.url()
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url + "emoji/image/lg/" + emoji) as request:
-                    if request.status == 404:
-                        return await ctx.send(
-                            "Sorry, I couldn't find that emoji from my API."
-                        )
-                    response = await request.json()
-                    embed = discord.Embed(color=await ctx.embed_colour())
-                    embed.set_image(url=response["url"])
-                    await ctx.send(embed=embed)
-        except aiohttp.ClientConnectionError:
-            await ctx.send("Sorry, the API isn't set up correctly.")
+        await self.get_emoji(ctx, "lg", emoji)
 
     @getemoji.command()
     @commands.bot_has_permissions(embed_links=True)
-    async def htc(self, ctx, emoji: str):
+    async def htc(self, ctx, *, emoji: str):
         """get an image of a htc emoji"""
-        await ctx.trigger_typing()
-        url = await self.config.url()
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url + "emoji/image/htc/" + emoji) as request:
-                    if request.status == 404:
-                        return await ctx.send(
-                            "Sorry, I couldn't find that emoji from my API."
-                        )
-                    response = await request.json()
-                    embed = discord.Embed(color=await ctx.embed_colour())
-                    embed.set_image(url=response["url"])
-                    await ctx.send(embed=embed)
-        except aiohttp.ClientConnectionError:
-            await ctx.send("Sorry, the API isn't set up correctly.")
+        await self.get_emoji(ctx, "htc", emoji)
 
     @getemoji.command()
     @commands.bot_has_permissions(embed_links=True)
-    async def softbank(self, ctx, emoji: str):
+    async def softbank(self, ctx, *, emoji: str):
         """get an image of a softbank emoji"""
-        await ctx.trigger_typing()
-        url = await self.config.url()
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(
-                    url + "emoji/image/softbank/" + emoji
-                ) as request:
-                    if request.status == 404:
-                        return await ctx.send(
-                            "Sorry, I couldn't find that emoji from my API."
-                        )
-                    response = await request.json()
-                    embed = discord.Embed(color=await ctx.embed_colour())
-                    embed.set_image(url=response["url"])
-                    await ctx.send(embed=embed)
-        except aiohttp.ClientConnectionError:
-            await ctx.send("Sorry, the API isn't set up correctly.")
+        await self.get_emoji(ctx, "softbank", emoji)
 
     @getemoji.command()
     @commands.bot_has_permissions(embed_links=True)
-    async def docomo(self, ctx, emoji: str):
+    async def docomo(self, ctx, *, emoji: str):
         """get an image of a docomo emoji"""
-        await ctx.trigger_typing()
-        url = await self.config.url()
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url + "emoji/image/docomo/" + emoji) as request:
-                    if request.status == 404:
-                        return await ctx.send(
-                            "Sorry, I couldn't find that emoji from my API."
-                        )
-                    response = await request.json()
-                    embed = discord.Embed(color=await ctx.embed_colour())
-                    embed.set_image(url=response["url"])
-                    await ctx.send(embed=embed)
-        except aiohttp.ClientConnectionError:
-            await ctx.send("Sorry, the API isn't set up correctly.")
+        await self.get_emoji(ctx, "docomo", emoji)
 
     @getemoji.command()
     @commands.bot_has_permissions(embed_links=True)
-    async def mozilla(self, ctx, emoji: str):
+    async def mozilla(self, ctx, *, emoji: str):
         """get an image of a mozilla emoji"""
-        await ctx.trigger_typing()
-        url = await self.config.url()
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url + "emoji/image/mozilla/" + emoji) as request:
-                    if request.status == 404:
-                        return await ctx.send(
-                            "Sorry, I couldn't find that emoji from my API."
-                        )
-                    response = await request.json()
-                    embed = discord.Embed(color=await ctx.embed_colour())
-                    embed.set_image(url=response["url"])
-                    await ctx.send(embed=embed)
-        except aiohttp.ClientConnectionError:
-            await ctx.send("Sorry, the API isn't set up correctly.")
+        await self.get_emoji(ctx, "mozille", emoji)
 
     @getemoji.command()
     @commands.bot_has_permissions(embed_links=True)
-    async def messenger(self, ctx, emoji: str):
+    async def messenger(self, ctx, *, emoji: str):
         """get an image of a messenger emoji"""
+        await self.get_emoji(ctx, "messenger", emoji)
+
+    async def get_emoji(self, ctx, vendor, emoji):
         await ctx.trigger_typing()
         url = await self.config.url()
         try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(
-                    url + "emoji/image/messenger/" + emoji
-                ) as request:
-                    if request.status == 404:
-                        return await ctx.send(
-                            "Sorry, I couldn't find that emoji from my API."
-                        )
-                    response = await request.json()
-                    embed = discord.Embed(color=await ctx.embed_colour())
-                    embed.set_image(url=response["url"])
-                    await ctx.send(embed=embed)
-        except aiohttp.ClientConnectionError:
-            await ctx.send("Sorry, the API isn't set up correctly.")
+            async with self.session.get(
+                url + f"emoji/image/{vendor}/" + emoji
+            ) as request:
+                if request.status == 404:
+                    await ctx.send(
+                        "Sorry, I couldn't find that emoji from my API."
+                    )
+                    return
+                response = await request.json()
+                embed = discord.Embed(color=await ctx.embed_colour())
+                embed.set_image(url=response["url"])
+                await ctx.send(embed=embed)
+        except:
+            await ctx.send("Uh oh, an error occured. Make sure the API is listening on the correct port.")
