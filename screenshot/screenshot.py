53c1da26cef4bf65f8234ba7977ea1c2baf6e67e
alec <50505980+aleclol@users.noreply.github.com>
2021-05-05 12:34:53 -0400

    ok

--- screenshot/screenshot.py
+++ screenshot/screenshot.py
@@ -12,12 +12,16 @@ class Screenshot(commands.Cog):
     @commands.bot_has_permissions(attach_files=True)
     @commands.command(aliases=["ss"])
     @commands.is_owner()
-    async def screenshot(self, ctx, link: str, wait: int = 0):
-        """Screenshots a given link."""
+    async def screenshot(self, ctx, link: str, wait: int = 3):
+        """
+        Screenshots a given link.
+        If no time is given, it will wait 3 seconds to screenshot
+        """
 
         await ctx.trigger_typing()
         browser = await launch()
         page = await browser.newPage()
+        await page.setViewport({'width':1280, 'height':720})
         try:
             await page.goto(link)
         except pyppeteer.page.PageError:
