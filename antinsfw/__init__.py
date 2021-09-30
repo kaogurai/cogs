b1a698f0ca6ad9852cd959dee9fdf61ee95f61fa
alec <50505980+aleclol@users.noreply.github.com>
2021-09-30 19:09:22 -0400

    remove antinsfw cog

--- antinsfw/__init__.py
+++
@@ -1,9 +0,0 @@
-from .antinsfw import AntiNSFW
-
-
-async def setup(bot) -> None:
-    cog = AntiNSFW(bot)
-    bot.add_cog(cog)
-
-
-__red_end_user_data_statement__ = "This cog does not store any end user data."
