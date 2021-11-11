154259ff7712d84da9e40fc8928b7f56d951100b
aleclol <50505980+aleclol@users.noreply.github.com>
2021-11-10 22:24:44 -0500

    [KaoTools] Invite

--- kaotools/kaotools.py
+++ kaotools/kaotools.py
@@ -196,7 +196,7 @@ class KaoTools(commands.Cog):
         Invite me or another bot!
         """
         if bot is None:
-            m = "Join the [support server](https://discord.gg/p6ehU9qhg8) for instructions on how to apply to invite kaogurai."
+            m = f"Click [here]({await self.invite_url()}) to invite me.\n\nNote: You need 75 members and 50% of your member count must be human."
             embed = discord.Embed(color=await ctx.embed_color(), description=m)
             await ctx.send(embed=embed)
             return
