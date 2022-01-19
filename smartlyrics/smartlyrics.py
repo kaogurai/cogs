30e1c5f1358ae08fc91956f3f4b59e12660b8dd8
aleclol <50505980+aleclol@users.noreply.github.com>
2022-01-18 20:27:08 -0500

    [SmartLyrics] custom api

--- smartlyrics/smartlyrics.py
+++ smartlyrics/smartlyrics.py
@@ -15,7 +15,7 @@ class SmartLyrics(commands.Cog):
     Gets lyrics for your current song.
     """
 
-    __version__ = "1.0.0"
+    __version__ = "1.1.0"
 
     def __init__(self, bot):
         self.bot = bot
@@ -40,20 +40,66 @@ class SmartLyrics(commands.Cog):
         return f"{pre_processed}\n\nCog Version: {self.__version__}"
 
     async def get_lyrics(self, query):
-        c = Deezer()
-        tracks = await c.search("track", query)
-        if not tracks:
-            await c.http.close()
+        initial_params = {
+            "method": "deezer.getUserData",
+            "input": 3,
+            "api_version": "1.0",
+            "api_token": "",
+        }
+        async with self.session.get(
+            "https://www.deezer.com/ajax/gw-light.php", params=initial_params
+        ) as r:
+            if r.status != 200:
+                return
+            json = await r.json()
+            api_token = json["results"]["checkForm"]
+
+        search_params = {
+            "method": "deezer.pageSearch",
+            "input": 3,
+            "api_version": "1.0",
+            "api_token": api_token,
+        }
+        search_data = {"query": query, "start": 0, "nb": 5, "top_tracks": True}
+
+        async with self.session.post(
+            f"https://www.deezer.com/ajax/gw-light.php?",
+            params=search_params,
+            json=search_data,
+        ) as r:
+            if r.status != 200:
+                return
+            search_results_data = await r.json()
+
+        search_results = search_results_data["results"]["TRACK"]["data"]
+        if not search_results:
             return
-        track = tracks[0]
-        lyrics = await c.api("song.getLyrics", {"sng_id": track["SNG_ID"]})
-        await c.http.close()
-        if not lyrics["results"]:
+
+        track = search_results[0]
+
+        lyrics_params = {
+            "method": "song.getLyrics",
+            "input": 3,
+            "api_version": "1.0",
+            "api_token": api_token,
+        }
+        lyrics_data = {"sng_id": track["SNG_ID"]}
+        async with self.session.post(
+            f"https://www.deezer.com/ajax/gw-light.php?",
+            params=lyrics_params,
+            json=lyrics_data,
+        ) as r:
+            if r.status != 200:
+                return
+            lyrics_data = await r.json()
+
+        if not lyrics_data["results"]:
             return
+
         artid = track["ALB_PICTURE"]
         artwork = f"https://e-cdn-images.dzcdn.net/images/cover/{artid}/264x264-000000-80-0-0.jpg"
         return (
-            lyrics["results"]["LYRICS_TEXT"],
+            lyrics_data["results"]["LYRICS_TEXT"],
             track["SNG_TITLE"],
             track["ART_NAME"],
             artwork,
