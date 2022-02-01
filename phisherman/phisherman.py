7a6ff173c921982d5a2d673815eb0207f48f9342
aleclol <50505980+aleclol@users.noreply.github.com>
2022-01-31 23:27:29 -0500

    [PhisherMan] stuff

--- phisherman/phisherman.py
+++ phisherman/phisherman.py
@@ -12,11 +12,22 @@ class PhisherMan(commands.Cog):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=73835)
         self.config.register_guild(
-            enabled=False, suspicious_ban=False, malicious_ban=True
+            enabled=False, safe="ignore", suspicious="delete", malicious="ban"
         )
+        self.bot.loop.create_task(self.set_token())
+
+    async def set_token(self):
+        token = await self.bot.get_shared_api_tokens("phisherman")
+        self.key = token.get("key")
+
+    @commands.Cog.listener()
+    async def on_red_api_tokens_update(self, service_name, api_tokens):
+        if service_name == "phisherman":
+            self.key = api_tokens.get("key")
 
     @commands.command(aliases=["checkforphish", "checkscam", "checkforscam"])
     @commands.bot_has_permissions(embed_links=True)
+    @commands.cooldown(1, 30, commands.BucketType.user)
     async def checkphish(self, ctx, url: str):
         """
         Check if a url is a phishing scam.
@@ -27,6 +38,5 @@ class PhisherMan(commands.Cog):
     @commands.is_admin_or_permissions(manage_guild=True)
     async def phisherman(self, ctx):
         """
-        Settings to set up phisherman.
+        Settings to set the phisherman integration.
         """
-        pass
