512c4f608f950ac4e94dd930e7f042f85a13f852
aleclol <50505980+aleclol@users.noreply.github.com>
2022-02-04 17:36:11 -0500

    [Wombo] im stupid

--- wombo/wombo.py
+++ wombo/wombo.py
@@ -64,7 +64,7 @@ class Wombo(commands.Cog):
                     resp = await req.text()
                     resp = json.loads(resp)
                     if "nudity_percentage" in resp:
-                        return resp["nudity_percentage"] > 0.1
+                        return resp["nudity_percentage"] < 0.25
         except aiohttp.ClientError:
             pass
         return False
